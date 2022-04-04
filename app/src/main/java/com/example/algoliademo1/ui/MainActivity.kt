46e21515fcdc7cf9259800d5fa4323bbb5e4f189
keerthi-con927 <keerthik97117@gmail.com>
2022-04-04 18:17:52 +0530

    before nav graph

--- app/src/main/java/com/example/algoliademo1/ui/MainActivity.kt
+++ app/src/main/java/com/example/algoliademo1/ui/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.Toast
 import com.example.algoliademo1.R
 import com.example.algoliademo1.data.source.local.entity.Order
+import com.example.algoliademo1.databinding.ActivityMainBinding
 import com.example.algoliademo1.ui.address.AddressFragment
 import com.example.algoliademo1.ui.cart.CartFragment
 import com.example.algoliademo1.ui.filters.FacetFragment
@@ -14,23 +17,58 @@ import com.example.algoliademo1.ui.orderdetail.OrderDetailFragment
 import com.example.algoliademo1.ui.products.ProductFragment
 import com.example.algoliademo1.ui.prductdetail.ProductDetailFragment
 import com.example.algoliademo1.ui.wishlist.WishlistFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.DocumentReference
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate: 1")
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate:2 ")
-        setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate:3 ")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         showProductFragment()
+
+        val navigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.home -> {
+                        Log.d(TAG, "onCreate: inside home")
+                        showProductFragment()
+                        true
+                    }
+                    R.id.cart -> {
+                        Log.d(TAG, "onCreate: inside cart")
+                        //Toast.makeText(this, "cart clicked", Toast.LENGTH_SHORT).show()
+                        showCartFragment()
+                        true
+                    }
+                    R.id.wishlist -> {
+                        showWishlistFragment()
+                        true
+                    }
+                    R.id.orders -> {
+                        showOrdersFragment()
+                        true
+
+                    }
+                    else -> super.onOptionsItemSelected(menuItem)
+                }
+
+            }
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
+
     }
 
     fun showProductFragment() {
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.container, ProductFragment())
+            .replace(R.id.container, ProductFragment())
+            //.addToBackStack("Products page")
             .commit()
     }
 
@@ -38,11 +76,11 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, FacetFragment())
-            .addToBackStack("facet")
+            //.addToBackStack("facet")
             .commit()
     }
 
-    fun showProductDetailFragment(id: String){
+    fun showProductDetailFragment(id: String) {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, ProductDetailFragment(id))
@@ -50,7 +88,7 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    fun showCartFragment(){
+    fun showCartFragment() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, CartFragment())
@@ -58,34 +96,36 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    fun showWishlistFragment(){
+    fun showWishlistFragment() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, WishlistFragment())
-            .addToBackStack("Cart items")
+            //.addToBackStack("Cart items")
             .commit()
     }
 
-    fun showAddressFragment(){
+    fun showAddressFragment() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, AddressFragment())
-            .addToBackStack("Address fragment")
+            //.addToBackStack("Address fragment")
             .commit()
     }
 
-    fun showOrdersFragment(){
+    fun showOrdersFragment() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, OrdersFragment())
-            .addToBackStack("Orders fragment")
+            //.addToBackStack("Orders fragment")
             .commit()
     }
 
     fun showOrderDetailFragment(order: Order) {
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.container, OrderDetailFragment(order))  // replace document reference with order
+            .replace(
+                R.id.container, OrderDetailFragment(order)
+            )  // replace document reference with order
             .addToBackStack("Order Detail fragment")
             .commit()
 
